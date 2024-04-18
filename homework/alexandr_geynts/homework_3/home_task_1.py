091b2c8d520e0edab044a98dbd0c4c6461d841b0
Aleksandr Geynts <a.geynts@eapteka.ru>
2024-04-18 18:26:38 +0600

    added homework

---
+++ homework/alexandr_geynts/homework_3/home_task_1.py
@@ -0,0 +1,14 @@
+a = 10
+b = 5
+
+# Сумма
+sum_result = a + b
+print("Сумма:", sum_result)
+
+# Разность
+difference_result = a - b
+print("Разность:", difference_result)
+
+# Произведение
+product_result = a * b
+print("Произведение:", product_result)
