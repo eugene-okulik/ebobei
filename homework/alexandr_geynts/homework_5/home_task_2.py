254aaff42594de2a3b877134a1068d6b83187bbb
Aleksandr Geynts <a.geynts@eapteka.ru>
2024-04-23 17:10:00 +0600

    added homework_5

--- homework/alexandr_geynts/homework_5/home_task_2.py
+++ homework/alexandr_geynts/homework_5/home_task_2.py
@@ -4,9 +4,13 @@ results = [
     "результат работы программы: 9"
 ]
 
-result_1 = int(results[0][results[0].find(":") + 1:].strip()) + 10
-result_2 = int(results[1][results[1].find(":") + 1:].strip()) + 10
-result_3 = int(results[2][results[2].find(":") + 1:].strip()) + 10
+result1 = results[0]
+result2 = results[1]
+result3 = results[2]
+
+result_1 = int(result1[result1.index(':') + 2:]) + 10
+result_2 = int(result2[result2.index(':') + 2:]) + 10
+result_3 = int(result3[result3.index(':') + 2:]) + 10
 
 print("Результат 1:", result_1)
 print("Результат 2:", result_2)
